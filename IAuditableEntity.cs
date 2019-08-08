b1693069da0afc3c180cf0b348ee44e7853710ee
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:09:18 -0700

    Add files via upload

---
+++ IAuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Penguin.Entities.Abstractions
+{
+    /// <summary>
+    /// An interface containing information for an entity that changes over time, and is auditable
+    /// </summary>
+    public interface IAuditableEntity : IEntity
+    {
+        /// <summary>
+        /// The last time this object was modified
+        /// </summary>
+        DateTime? DateModified { get; }
+    }
+}
---
+++ IEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Penguin.Entities.Abstractions
+{
+    /// <summary>
+    /// An interface for the lowest intended top level objects used by the penguin stack of persistence
+    /// </summary>
+    public interface IEntity
+    {
+        #region Properties
+
+        /// <summary>
+        /// The intended key of the entity
+        /// </summary>
+        int _Id { get; }
+
+        /// <summary>
+        /// A Globally unique identifier for the entity
+        /// </summary>
+        Guid Guid { get; }
+
+        #endregion Properties
+    }
+}
